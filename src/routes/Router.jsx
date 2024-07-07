e4e8b157864c440313efa6c1d55ed2ff4552d7fb
Takumi1217 <im12u71ka@gmail.com>
2024-07-07 20:24:19 +0900

    3(midway)

---
+++ src/routes/Router.jsx
@@ -0,0 +1,19 @@
+// Router.jsx
+
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../Home";
+import Signup from "../Signup";
+import Login from "../Login";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+};
+
+export default App;
